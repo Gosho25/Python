3dd9d58dce2cbeeaee537980fd1db23281c0942b
Bat_Gosho <goshkodark25@gmail.com>
2024-11-28 19:23:35 +0200

    Add files via upload

--- zad3.py
+++ zad3.py
@@ -1,7 +1,32 @@
-with open('log.txt', 'r') as l:
-    lines = l.readlines()
-    error_count = 0
-    for line in lines:
-        if "error" in line.lower():
-            error_count += 1          
-    print(f"{error_count}")            
+class TemperatureConverter:
+    def __init__(self, temperature, scale="Celsius"):
+        self.temperature = temperature
+        self.scale = scale.lower()
+
+    def celsius_to_fahrenheit(self):
+        if self.scale == "celsius":
+            return (self.temperature * 9/5) + 32
+        else:
+            return "Температурата трябва да бъде в Целзий."
+
+    def fahrenheit_to_celsius(self):
+        if self.scale == "fahrenheit":
+            return (self.temperature - 32) * 5/9
+        else:
+            return "Температурата трябва да бъде във Фаренхайт."
+
+    def set_temperature(self, temperature, scale="Celsius"):
+        self.temperature = temperature
+        self.scale = scale.lower()
+
+temp1 = TemperatureConverter(25, "Celsius")
+print(f"25 Целзий в Фаренхайт: {temp1.celsius_to_fahrenheit()}°F")
+
+temp2 = TemperatureConverter(77, "Fahrenheit")
+print(f"77 Фаренхайт в Целзий: {temp2.fahrenheit_to_celsius()}°C")
+
+temp1.set_temperature(100, "Celsius")
+print(f"100 Целзий в Фаренхайт: {temp1.celsius_to_fahrenheit()}°F")
+
+temp2.set_temperature(212, "Fahrenheit")
+print(f"212 Фаренхайт в Целзий: {temp2.fahrenheit_to_celsius()}°C")
