0e59fe9ea4db5a5a0a0d342d717163efe7a35012
Bat_Gosho <goshkodark25@gmail.com>
2024-12-01 00:30:32 +0200

    Add files via upload

---
+++ turtle.py
@@ -0,0 +1,13 @@
+import turtle
+
+t = turtle.Turtle()
+t.shape('turtle')
+t.color('green')
+t.pensize(5)
+t.forward(200)
+t.right(120) 
+t.forward(200)
+t.right(120) 
+t.forward(200)
+t.right(120) 
+#Shte raboti ako e NE E SAVNATA
