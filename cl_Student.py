46d055be7609c1310c52c9f2301656540e310f90
Bat_Gosho <goshkodark25@gmail.com>
2024-12-11 16:52:18 +0200

    Update cl_Student.py

--- cl_Student.py
+++ cl_Student.py
@@ -5,5 +5,5 @@ class Student:
         self.success = float(success)
 student1 = Student("Ivan", 2, 4.50)
 print(student1.name) 
-print(student1.group)
+print(student1.group) 
 print(student1.success) 
